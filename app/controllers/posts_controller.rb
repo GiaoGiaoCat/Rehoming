00a3b85ded81769b00a2f6840420ad0e1699ea6e
雁鸣 <cisolarix@gmail.com>
2017-05-04 21:45:24 +0800

    ADD: 帖子控制器

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  # before_action :login_required
+
+  def create
+    build_post
+    if @post.save
+      render json: @post, status: :created, include: [:attachments]
+    else
+      render json: @post.errors.messages, status: :bad_request
+    end
+  end
+
+  private
+
+  def build_post
+    @post = Post.new
+    @post.attributes = post_params
+  end
+
+  def post_params
+    params.fetch(:post, {}).permit(
+      :content, attachments_attributes: [:category, :url]
+    )
+  end
+end
