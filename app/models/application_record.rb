45036b65d735e144ceb07c99eae6b88baa14d66b
Victor Wang <wjp2013@gmail.com>
2017-05-06 06:01:49 +0800

    增加 encrypted id 模块

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  include EncryptedId
+  encrypted_id key: 'kwXKxc3zRH3UFz'
 end
---
+++ app/models/concerns/encrypted_id.rb
@@ -0,0 +1,77 @@
+module EncryptedId
+  extend ActiveSupport::Concern
+
+  CIPHER_NAME = 'aes-256-cbc'.freeze
+  CIPHER_IV = ['1e5673b2572af26a8364a50af84c7d2a'].pack('H*')
+
+  included do
+  end
+
+  module ClassMethods
+    def encrypted_id(options = {})
+      cattr_accessor :encrypted_id_key
+      self.encrypted_id_key = Digest::SHA256.digest(options[:key] || encrypted_id_default_key)
+      define_singleton_method(:find_single, -> { logger.info 'foo' })
+    end
+
+    def find(*args)
+      scope = args.slice!(0)
+      options = args.slice!(0) || {}
+      if !(scope.is_a? Symbol) && encrypted_id? && !options[:no_encrypted_id] && scope.to_s !~ /\A\d+\z/
+        begin
+          scope =
+            if scope.is_a? Array
+              scope.map! { |encrypted_id| decrypt(encrypted_id_key, encrypted_id) }
+            else
+              decrypt(encrypted_id_key, scope.to_s)
+            end
+        rescue OpenSSL::Cipher::CipherError
+          raise ActiveRecord::RecordNotFound.new("Could not decrypt ID #{scope}")
+        end
+      end
+      options.delete(:no_encrypted_id)
+      super(scope)
+    end
+
+    def find_by_encrypted_id(*args)
+      find(*args)
+    end
+
+    def encrypted_id?
+      true
+    end
+
+    def encrypted_id_default_key
+      name
+    end
+
+    def decrypt(key, id)
+      c = OpenSSL::Cipher.new(CIPHER_NAME).decrypt
+      c.iv = CIPHER_IV
+      c.key = key
+      c.update([id].pack('H*')) + c.final
+    end
+
+    def encrypt(key, id)
+      c = OpenSSL::Cipher.new(CIPHER_NAME).encrypt
+      c.iv = CIPHER_IV
+      c.key = key
+      (c.update(id.to_s) + c.final).unpack('H*')[0]
+    end
+  end
+
+  def to_param
+    self.class.encrypt(self.class.encrypted_id_key, id)
+  end
+
+  def to_key
+    key = id || nil
+    key = [self.class.encrypt(self.class.encrypted_id_key, id)] if key
+    key
+  end
+
+  def reload(options = nil)
+    options = (options || {}).merge(no_encrypted_id: true)
+    super(options)
+  end
+end
