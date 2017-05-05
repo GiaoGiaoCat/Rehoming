45d74cfb889b190eea770681debde136bd55f659
雁鸣 <cisolarix@gmail.com>
2017-05-05 17:29:27 +0800

    CHG: 默认中文

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Rehoming
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.i18n.default_locale = :'zh-CN'
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
