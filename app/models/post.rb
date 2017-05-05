49ee66628096623b113538025d97d1659e6db42f
雁鸣 <cisolarix@gmail.com>
2017-05-05 17:17:47 +0800

    ADD: 增加 post 的限制及测试

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,17 @@ class Post < ApplicationRecord
   has_many :attachments
 
   validates :content, presence: true, length: { in: 1..10_000 }
+  validate :images_limitation, :video_limitation
 
   accepts_nested_attributes_for :attachments
+
+  private
+
+  def images_limitation
+    errors.add :base, :too_many_images if attachments.select(&:image?).count > 9
+  end
+
+  def video_limitation
+    errors.add :base, :too_many_videos if attachments.select(&:video?).count > 1
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,4 +1,47 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  # validates :content, presence: true, length: { in: 1..10_000 }
+  # validate :images_limitation, :video_limitation
+  test '验证内容' do
+    p = Post.new content: ''
+    assert_not p.valid?
+
+    p.content = '1'
+    assert p.valid?
+
+    p.content = '1' * 10_000
+    assert p.valid?
+
+    p.content = '1' * 10_001
+    assert_not p.valid?
+  end
+
+  test '验证图片附件' do
+    p = Post.new content: 'content goes here'
+
+    9.times do |i|
+      p.attachments << Attachment.new(category: 'image', url: "#{i}*100")
+    end
+    assert_nothing_raised do
+      p.save!
+    end
+    p.attachments << Attachment.new(category: 'image', url: 'url goes here')
+    assert_not p.valid?
+    assert p.errors.key?(:base)
+  end
+
+  test '验证视频附件' do
+    p = Post.new content: 'content goes here'
+
+    1.times do |i|
+      p.attachments << Attachment.new(category: 'video', url: "#{i}*100")
+    end
+    assert_nothing_raised do
+      p.save!
+    end
+    p.attachments << Attachment.new(category: 'video', url: 'url goes here')
+    assert_not p.valid?
+    assert p.errors.key?(:base)
+  end
 end
