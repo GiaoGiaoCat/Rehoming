b34a9b18bae2c93b578fc65d1e3208633264957e
Victor Wang <wjp2013@gmail.com>
2017-05-06 15:38:14 +0800

    serializer 模板返回加密后的 id

--- app/models/user/sign_in.rb
+++ app/models/user/sign_in.rb
@@ -43,8 +43,7 @@ class User::SignIn < ActiveType::Object
 
   def ensure_auth_token_has_a_value
     return unless user
-    # payload = { user_id: user.to_param, exp: 30.days.from_now.to_i }
-    payload = { user_id: user.id, exp: 30.days.from_now.to_i }
+    payload = { user_id: user.to_param, exp: 30.days.from_now.to_i }
     self.auth_token = JsonWebToken.encode(payload)
   end
 
