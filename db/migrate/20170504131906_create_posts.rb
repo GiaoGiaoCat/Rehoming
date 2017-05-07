2b1ccad88be79e2d55b2fb7db2ef4da63a4876d6
雁鸣 <cisolarix@gmail.com>
2017-05-07 23:37:30 +0800

    CHG: post

--- db/migrate/20170504131906_create_posts.rb
+++ db/migrate/20170504131906_create_posts.rb
@@ -1,8 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.text :content, limit: 64.kilobytes
+      t.integer :group_id
+      t.integer :user_id
+      t.text    :content, limit: 64.kilobytes
       t.timestamps null: false
     end
+
+    add_index :posts, [:group_id, :user_id], name: 'posts_index'
   end
 end
