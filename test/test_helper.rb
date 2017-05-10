05e98a1bdee32b5c71dee2ae772ab2e5fec5e23a
Victor Wang <wjp2013@gmail.com>
2017-05-10 20:59:58 +0800

    Setting Up Test Coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'support/custom_header_setup'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/vendor/' # Ignores any file containing "/vendor/" in its path.
+  # add_filter "/lib/myfile.rb" # Ignores a specific file.
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
