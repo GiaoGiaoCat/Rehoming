f069b8f13b83f473c728ff474267e31b31e69633
Victor Wang <wjp2013@gmail.com>
2017-05-13 19:54:42 +0800

    单元测试

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -5,6 +5,7 @@ class CommentTest < ActiveSupport::TestCase
     @victor = users(:victor)
     @post = posts(:one)
     @comment = comments(:one)
+    @new_comment = Comment.new(commentable: @post, user: @victor)
   end
 
   test '验证必填项' do
@@ -13,6 +14,18 @@ class CommentTest < ActiveSupport::TestCase
     assert f.errors.key? :user
     assert f.errors.key? :content
     assert f.errors.key? :commentable
+  end
+
+  test '携带附件时评论内容可以为空' do
+    @new_comment.attachments.new(url: 'http://www.baidu.com/hello.jpg', category: 10)
+    assert @new_comment.valid?
+    assert @new_comment.save
+  end
+
+  test '携带附件数量不能超过1个' do
+    @new_comment.attachments.new(url: 'http://www.baidu.com/hello.jpg', category: 10)
+    @new_comment.attachments.new(url: 'http://www.baidu.com/yeah.jpg', category: 10)
+    refute @new_comment.valid?
   end
 
   test '可以对 post 进行回复' do
