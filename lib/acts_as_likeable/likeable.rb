ebe692d77fbf37f533ae83ed43148741fcb8142b
Victor Wang <wjp2013@gmail.com>
2017-05-14 11:03:25 +0800

    fix code climate

--- lib/acts_as_likeable/likeable.rb
+++ lib/acts_as_likeable/likeable.rb
@@ -3,7 +3,7 @@ module ActsAsLikeable
     extend ActiveSupport::Concern
 
     included do
-      has_many :likes, as: :likeable
+      has_many :likes, as: :likeable, dependent: :destroy
     end
 
     def likeable?
