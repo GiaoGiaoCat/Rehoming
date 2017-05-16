bb912962deda003afb1d2447d33bca5a301fe6d7
Victor Wang <wjp2013@gmail.com>
2017-05-16 18:29:41 +0800

    帖子详情返回作者信息

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     load_post
-    render json: @post, include: [comments: [:attachments, :comments]]
+    render json: @post, include: [:author, comments: %i(attachments comments)]
   end
 
   def create
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,7 @@ class PostSerializer < ApplicationSerializer
   type 'posts'
   attributes :content
 
+  belongs_to :author, serializer: AuthorSerializer
   has_many :attachments
   has_many :comments
 end
