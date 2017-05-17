4973743e105c548d72dcb8e59c4ae3e90ab82b2f
雁鸣 <cisolarix@gmail.com>
2017-05-17 23:13:56 +0800

    FIX: posts 控制器测试

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -7,6 +7,7 @@ class AuthorSerializer < ApplicationSerializer
 
   def nickname
     return nickname unless group
-    object.group_enrollments.find_by(group_id: group.id).nickname || nickname
+    object.group_enrollments.find_by(group_id: group.id)&.nickname ||
+      object.nickname
   end
 end
