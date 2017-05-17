e73be87f34e78ca6783df3de078b07dbb371a428
雁鸣 <cisolarix@gmail.com>
2017-05-17 23:28:25 +0800

    FIX: 修复

--- test/unit/lib/acts_as_pinable_test.rb
+++ test/unit/lib/acts_as_pinable_test.rb
@@ -3,12 +3,9 @@ require 'test_helper'
 class ActsAsPinableTest < ActiveSupport::TestCase
   def setup
     @victor = users(:victor)
-    @group = groups(:one)
     @post_one = posts(:one)
     @post_two = posts(:two)
     @comment = comments(:one)
-    byebug
-    @victor.pin @post_one
   end
 
   test '验证 pinable? 方法正确' do
@@ -22,23 +19,26 @@ class ActsAsPinableTest < ActiveSupport::TestCase
     assert_difference 'Post.where(sticky: true).count' do
       @victor.pin @post_two
     end
+
+    @victor.pin @post_one
     assert_difference 'Post.where(sticky: true).count', -1 do
-      @victor.dislike @post_one
+      @victor.unpin @post_one
     end
   end
-  #
-  # test '不同重复赞同一个帖子或评论' do
-  #   assert_difference '@victor.likes.count', 0 do
-  #     @victor.like @post_one
-  #   end
-  # end
-  #
-  # test '用户对评论可以赞和取消赞' do
-  #   assert_difference '@victor.likes.count' do
-  #     @victor.like @comment
-  #   end
-  #   assert_difference '@victor.likes.count', -1 do
-  #     @victor.dislike @comment
-  #   end
-  # end
+
+  test '不能重复赞同一个帖子或评论' do
+    @victor.like @post_one
+    assert_difference '@victor.likes.count', 0 do
+      @victor.like @post_one
+    end
+  end
+
+  test '用户对评论可以赞和取消赞' do
+    assert_difference '@victor.likes.count' do
+      @victor.like @comment
+    end
+    assert_difference '@victor.likes.count', -1 do
+      @victor.dislike @comment
+    end
+  end
 end
