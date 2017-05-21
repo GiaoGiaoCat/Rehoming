6de45272d9a4b3fc37b9452f207baa29fcfe8062
Victor Wang <wjp2013@gmail.com>
2017-05-21 18:15:21 +0800

    修改种子数据方便开发

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 20170514010031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_posts_on_group_id"
+    t.index ["recommended"], name: "index_posts_on_recommended"
+    t.index ["sticky"], name: "index_posts_on_sticky"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
