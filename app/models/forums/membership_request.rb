80e00a4d05e812e75baaa98609e0ffa374c07d7c
Victor Wang <wjp2013@gmail.com>
2017-05-26 16:00:54 +0800

    加入免审核小组自动通过审核

--- app/models/forums/membership_request.rb
+++ app/models/forums/membership_request.rb
@@ -1,12 +1,22 @@
 class Forums::MembershipRequest < ActiveType::Record[Forums::Membership]
   attribute :action, :string
 
-  validates :action, presence: true, inclusion: { in: %w(accept reject ignore) }
+  validates :action, presence: true, inclusion: { in: %w(accept reject ignore) }, allow_blank: true
 
   default_scope -> { pending }
+
+  before_create :auto_approval, unless: :membership_approval_needed?
+
+  delegate :membership_approval_needed?, to: :forum
 
   def update_status
     return false if invalid? || !respond_to?(action)
     send("#{action}!")
+  end
+
+  private
+
+  def auto_approval
+    accept
   end
 end
