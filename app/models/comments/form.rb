ffccad035e489afea0892c22d16713da3aed3832
雁鸣 <cisolarix@gmail.com>
2017-05-29 12:03:54 +0800

    ADD: 对 post 的 comment 回复时的动态

--- app/models/comments/form.rb
+++ app/models/comments/form.rb
@@ -1,12 +1,18 @@
 class Comments::Form < ActiveType::Record[Comment]
-  after_create :feed_author
+  after_create :feed
 
   private
 
-  def feed_author
+  def feed
     commentable.author.feeds.create(
       sourceable: self,
       event:      'new_comment_of_post'
+    )
+
+    return if replied_user_id.blank?
+    replied_user.feeds.create(
+      sourceable: self,
+      event:      'new_comment_of_comment'
     )
   end
 end
