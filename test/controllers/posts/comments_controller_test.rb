bc8604744145700efdd095603a7a19d49bd1359b
Victor Wang <wjp2013@gmail.com>
2017-05-30 17:50:59 +0800

    重构功能测试符合 5.1 写法

--- test/controllers/posts/comments_controller_test.rb
+++ test/controllers/posts/comments_controller_test.rb
@@ -1,70 +1,50 @@
 require 'test_helper'
 
-class Posts::CommentsControllerTest < ActionController::TestCase
-  def setup
+class Posts::CommentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
     @post = posts(:post_with_no_comment)
+    @comment = comments(:three)
   end
 
-  test '可以对 post 发表评论' do
-    assert_difference '@post.comments.count' do
-      post :create, params: {
-        post_id: @post.to_param,
-        data: {
-          type: 'comments',
-          attributes: { content: '合法数据' }
-        }
-      }
-      assert_response :success
-      assert_equal 201, @response.status
+  test 'create a comment to post' do
+    params_data = { data: { attributes: { content: '合法数据' } } }
+    assert_difference -> { @post.comments.count } do
+      post post_comments_url(@post), params: params_data, headers: @headers
     end
-  end
-
-  test '可以对 post 的评论者进行回复' do
-    post :create, params: {
-      post_id: @post.to_param,
-      data: {
-        type: 'comments',
-        attributes: { content: '合法数据' }
-      }
-    }
-    assert_response :success
-    assert_equal 201, @response.status
-    assert_equal 1, @post.comments.count
-
-    @headers = { 'HTTP_AUTHORIZATION' => JsonWebToken.issue(user_id: users(:yuki).id) }
-
-    post :create, params: {
-      post_id: @post.to_param,
-      data: {
-        type: 'comments',
-        attributes: { content: '真是合法数据吗？', replied_user_id: users(:victor).id }
-      }
-    }
 
     assert_response :success
     assert_equal 201, @response.status
-    assert_equal 2, @post.comments.count
-    assert_equal users(:victor).id, @post.comments.last.replied_user_id
   end
 
-  test '评论可以带附件' do
-    post :create, params: {
-      post_id: @post.to_param,
+  test 'comment with replyer' do
+    params_data = {
       data: {
-        type: 'comments',
+        attributes: { content: '合法数据', replied_user_id: @comment.author.id }
+      }
+    }
+    assert_difference -> { @post.comments.count } do
+      post post_comments_url(@post), params: params_data, headers: @headers
+    end
+
+    assert_response :success
+    assert_equal @comment.author.id, @post.comments.last.replied_user_id
+  end
+
+  test 'create a comment with attachments' do
+    params_data = {
+      data: {
         attributes: {
           content: '合法数据',
-          attachments_attributes: [{
-            category: 'image',
-            url: 'http://www.baidu.com/hello.jpg'
-          }]
+          attachments_attributes: [{ category: 'image', url: 'http://www.baidu.com/hello.jpg' }]
         }
       }
     }
+    assert_difference -> { @post.comments.count } do
+      post post_comments_url(@post), params: params_data, headers: @headers
+    end
+
     assert_response :success
-    assert_equal 201, @response.status
-    assert_equal 1, @post.comments.count
-    assert_equal 1, @post.comments.first.attachments.count
+    assert_not @post.comments.first.attachments.count.zero?
     assert_equal 'image', @post.comments.first.attachments.first.category
   end
 end
