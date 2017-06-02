a59765f1a6b2980e2fd9c379bbedfedccfc2d526
Victor Wang <wjp2013@gmail.com>
2017-06-02 19:29:49 +0800

    设置圈子可以发帖的用户组

--- app/controllers/forums/preferences_controller.rb
+++ app/controllers/forums/preferences_controller.rb
@@ -11,7 +11,7 @@ class Forums::PreferencesController < ApplicationController
   def preference_attrs
     %i(
       member_list_protected postable_until_tomorrow shared_post_allowed
-      direct_message_allowed membership_approval_needed postable_role
+      direct_message_allowed membership_approval_needed postable_roles
     )
   end
 end
--- test/models/forums/preference_test.rb
+++ test/models/forums/preference_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class Forums::PreferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validate postable_roles must of valid roles' do
+    preference = forums(:one).preference
+
+    preference.postable_roles = [10, 20]
+    assert preference.valid?
+
+    preference.postable_roles = [10, 90]
+    assert_not preference.valid?
+  end
 end
