fec45afa697a9807e0704b1a6081bf4908dcaa6e
Victor Wang <wjp2013@gmail.com>
2017-06-03 14:54:38 +0800

    重构 EncryptedId 去除重复代码

--- lib/rails_ext/activerecord_finder.rb
+++ lib/rails_ext/activerecord_finder.rb
@@ -1,16 +1,5 @@
 class ActiveRecord::Associations::CollectionProxy
   # NOTE: forum.posts.find('a0d3792d0dab6c6dea7b9de208dc3ea3') will called this method.
   #       just copy form `encrypted_id.rb`.
-  def find(*args)
-    scope = args.slice!(0)
-    options = args.slice!(0) || {}
-    if !options[:no_encrypted_id] && scope.to_s !~ /\A\d+\z/
-      begin
-        scope = process_scope(scope)
-      rescue OpenSSL::Cipher::CipherError
-        raise ActiveRecord::RecordNotFound.new("Could not decrypt ID #{scope}")
-      end
-    end
-    super(scope)
-  end
+  prepend RailsExt::ActiveRecordFinder
 end
