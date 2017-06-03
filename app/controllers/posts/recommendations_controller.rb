3483abbed9984784dbf8acc28ab7dd8b40642395
Victor Wang <wjp2013@gmail.com>
2017-06-03 23:46:18 +0800

    refactor

--- app/controllers/posts/recommendations_controller.rb
+++ app/controllers/posts/recommendations_controller.rb
@@ -1,15 +1,4 @@
 class Posts::RecommendationsController < ApplicationController
-  before_action :load_parent
-
-  def create
-    authorize @parent.forum, :manage_recommend?
-    @current_user.recommend(@parent)
-    head :created
-  end
-
-  def destroy
-    authorize @parent.forum, :manage_recommend?
-    @current_user.unrecommend(@parent)
-    head :no_content
-  end
+  include ActsAsAction
+  define_action_names verb: :recommend, unverb: :unrecommend, authorize_name: :manage_recommend?
 end
