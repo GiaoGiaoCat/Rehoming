624d49a468dda2f32fa2ff761deae0ec145db5b4
雁鸣 <cisolarix@gmail.com>
2017-06-05 22:40:04 +0800

    CHG: 尽量测试 token 的唯一性

--- test/models/forums/invitation_test.rb
+++ test/models/forums/invitation_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class Forums::InvitationTest < ActiveSupport::TestCase
   test 'ensure token' do
-    invitation = Forums::Invitation.new(forum: forums(:one))
-    assert invitation.save
-    assert invitation.token.present?
+    times = 100
+    assert_difference 'Forums::Invitation.count', times do
+      times.times do
+        invitation = Forums::Invitation.new(forum: forums(:one))
+        assert invitation.save
+        assert invitation.token.present?
+      end
+    end
   end
 end
