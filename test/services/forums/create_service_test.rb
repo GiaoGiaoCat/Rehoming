17cebecf476e9e94b2ebbf32f9904b88be5175e9
Victor Wang <wjp2013@gmail.com>
2017-06-07 15:03:11 +0800

    forum form 模型挪动到 services 中

--- test/services/forums/create_service_test.rb
+++ test/services/forums/create_service_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class Forums::FormTest < ActiveSupport::TestCase
+class Forums::CreateServiceTest < ActiveSupport::TestCase
   setup do
     @victor = users(:victor)
   end
 
   test '用户创建圈子后，自动成为圈主' do
     assert_difference -> { @victor.roles.count } do
-      Forums::Form.create(owner: @victor, name: 'test forum', category: 20)
+      Forums::CreateService.create(moderator: @victor, name: 'test forum', category: 20)
     end
     assert @victor.reload.has_role?(:moderator, Forum.last)
   end
