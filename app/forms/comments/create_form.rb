33d8cd2e9e82e2a7a93f20639c479a88d83bf10f
Victor Wang <wjp2013@gmail.com>
2017-06-14 00:13:26 +0800

    DRY

--- app/forms/comments/create_form.rb
+++ app/forms/comments/create_form.rb
@@ -1,10 +1,9 @@
 class Comments::CreateForm < ApplicationForm
+  include ValidPostingRights
+
   attribute :commentable
   attribute :author
   attribute :replied_user_id, :string
-
-  validate :user_should_in_forum
-  validate :postable_until_tomorrow
 
   Comment.column_names.each { |attr| delegate attr.to_sym, "#{attr}=".to_sym, to: :object }
   delegate :attachments_attributes=, to: :object
@@ -16,15 +15,6 @@ class Comments::CreateForm < ApplicationForm
   def setup_associations
     object.commentable = commentable
     object.author = author
-  end
-
-  def user_should_in_forum
-    errors.add :base, :not_in_forum unless author_membership
-  end
-
-  def postable_until_tomorrow
-    return unless forum.postable_until_tomorrow?
-    errors.add :base, :postable_until_tomorrow if author_membership.created_at.next_day > Time.current
   end
 
   def author_membership
--- app/forms/posts/create_form.rb
+++ app/forms/posts/create_form.rb
@@ -1,9 +1,9 @@
 class Posts::CreateForm < ApplicationForm
+  include ValidPostingRights
+
   attribute :forum
   attribute :author
 
-  validate :user_should_in_forum
-  validate :postable_until_tomorrow
   validate :author_role_can_post
 
   Post.column_names.each { |attr| delegate attr.to_sym, "#{attr}=".to_sym, to: :object }
@@ -15,15 +15,6 @@ class Posts::CreateForm < ApplicationForm
   def setup_associations
     object.forum = forum
     object.author = author
-  end
-
-  def user_should_in_forum
-    errors.add :base, :not_in_forum unless author_membership
-  end
-
-  def postable_until_tomorrow
-    return unless forum.postable_until_tomorrow?
-    errors.add :base, :postable_until_tomorrow if author_membership.created_at.next_day > Time.current
   end
 
   def author_role_can_post
