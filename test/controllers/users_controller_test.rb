faf18a887446d3f9e694c7cc0747798ec9694880
Victor Wang <wjp2013@gmail.com>
2017-06-19 18:04:09 +0800

    补全测试

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,10 +6,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should update current user profile' do
-    assert_changes -> { current_user.reload.nickname } do
-      params = { data: { attributes: { nickname: 'Forum one' } } }
-      patch me_users_url, params: params, headers: @headers
+  test 'should update current user profile and refresh cache' do
+    assert_changes -> { Rails.cache.read(current_user.to_param) } do
+      assert_changes -> { current_user.reload.nickname } do
+        params = { data: { attributes: { nickname: 'Forum one' } } }
+        patch me_users_url, params: params, headers: @headers
+      end
     end
     assert_response :success
   end
