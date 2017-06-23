6f7abd41e32b5b6475198af16cd5b20c720b8ca2
Victor Wang <wjp2013@gmail.com>
2017-06-23 17:27:54 +0800

    test cleanup feed job

--- test/jobs/feed_cleanup_job_test.rb
+++ test/jobs/feed_cleanup_job_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FeedCleanupJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    params = {
+      sourceable_id: posts(:one).id, sourceable_type: posts(:one).class,
+      creator_id: users(:roc).id, user_id: users(:victor).id,
+      event: 'new_post'
+    }
+    @feed = Feeds::CreateForm.create(params).object
+  end
+
+  test 'cleanup feed' do
+    assert_difference -> { users(:victor).feeds.value.size }, -1 do
+      FeedCleanupJob.perform_now(@feed.cache_key)
+    end
+  end
 end
