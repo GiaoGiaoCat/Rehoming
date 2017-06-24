b08b7ee55cb61cc73470cdc67ac0de8121e70a35
Victor Wang <wjp2013@gmail.com>
2017-06-24 14:51:47 +0800

    清理代码中的注释

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,33 @@
 class User < ApplicationRecord
-  # extends ...................................................................
   rolify
-  # includes ..................................................................
+
   include ActsAsLikeable::Liker
   include ActsAsFavorable::Favoriter
   include ActsAsPinable::Piner
   include ActsAsRecommendable::Recommender
   include Redis::Objects
-  # constants .................................................................
-  # relationships .............................................................
+
   has_many :posts
   has_many :favorite_posts, through: :favorites, source: :favorable, source_type: 'Post'
   has_many :forum_memberships, class_name: 'Forums::Membership', foreign_key: 'user_id'
   has_many :forums, -> { merge(Forums::Membership.active) }, through: :forum_memberships
   has_many :membership_requests, class_name: 'Forums::MembershipRequest', foreign_key: 'user_id'
   has_many :forum_preferences, class_name: 'Users::ForumPreference'
-  # validations ...............................................................
+
   validates :unionid, presence: true, uniqueness: true
   validates :nickname, presence: true
   validates :headimgurl, presence: true
-  # callbacks .................................................................
-  # scopes ....................................................................
+
   scope :blocked, -> { joins(:forum_memberships).merge(Forums::Membership.blocked) }
   scope :available, -> { joins(:forum_memberships).merge(Forums::Membership.available) }
   scope :feed_allowed, -> { joins(:forum_preferences).merge(Users::ForumPreference.feed_allowed) }
   scope :by_filter, ->(filter) { blocked if filter == 'blocked' }
-  # additional config (i.e. accepts_nested_attribute_for etc...) ..............
+
   encrypted_id key: 'gaeexiHdLTQ8Fg'
   serialize :raw_info, Hash
   counter :feeds_count
   list :feeds
-  # class methods .............................................................
-  # public instance methods ...................................................
+
   def join_forum(forum)
     rejoin_membership_or_create_membership_request(forum)
   end
@@ -50,8 +46,6 @@ class User < ApplicationRecord
     return nickname unless forum_membership
     forum_membership.preference.nickname
   end
-  # protected instance methods ................................................
-  # private instance methods ..................................................
 
   private
 
