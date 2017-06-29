10060e9fb24f05c7e44e7494456fe6374a4385e5
Victor Wang <wjp2013@gmail.com>
2017-06-29 23:19:51 +0800

    修复 forum id 错误

--- app/serializers/forums/post_preview_serializer.rb
+++ app/serializers/forums/post_preview_serializer.rb
@@ -3,10 +3,12 @@ class Forums::PostPreviewSerializer < ApplicationSerializer
 
   attributes :content
 
-  belongs_to :forum
+  belongs_to :forum_for_serializer, key: :forum
   belongs_to :author
   has_many :attachments
-  has_many :comments do
-    object.comments.limit(5)
+
+  class ForumSerializer < ApplicationSerializer
+    cache key: 'forum_preview'
+    attributes :name, :description, :cover, :background_color, :posts_count
   end
 end
